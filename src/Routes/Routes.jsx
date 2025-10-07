b2b171482e51655ce028046ce94dd08b37100eca
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-10-08 00:59:23 +0600

    projects setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from "react-router/dom";
+import App from '../App';
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+        {
+           index:true,
+           path:"/",
+           Component:Home,
+        },
+    ]
+  },
+]);
+
+export default router;
