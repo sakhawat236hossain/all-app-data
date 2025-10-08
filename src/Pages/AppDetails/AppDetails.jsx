afb3cd29781f3098cf54c89fd79dcf302e95be4b
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-10-09 00:53:10 +0600

    dainamik route

--- src/Pages/AppDetails/AppDetails.jsx
+++ src/Pages/AppDetails/AppDetails.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const AppDetails = () => {
+    const { id } = useParams(); 
+    const appId = parseInt(id)
+    const appData = useLoaderData();   
+    
+    const singleApp = appData.find(app => app.id === appId);
+    
+    console.log("id:", id);
+    console.log("App Data:", appData);
+    console.log("Single App:", singleApp);
+
+    if (!singleApp) return <p>App not found!</p>;
+
     return (
         <div>
-            app details
+            {/* img */}
+          <div>
+
+          </div>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -30,6 +30,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/appDetails/:id",
+        loader: () => fetch("/AppData.json"),
         Component: AppDetails,
       },
     ],
